aff911749d1e491ae893f21ab97866efcc3c7768
markskwid <bascal.markponce.f.1015@gmail.com>
2025-12-17 10:36:35 +0800

    Delete GET

--- app/api/cart/[id]/route.ts
+++ app/api/cart/[id]/route.ts
@@ -36,7 +36,7 @@ export async function PATCH(
   { params }: { params: { id: string } }
 ) {
   try {
-    const { id } = params;
+    const { id } = await params;
     const body = await request.json();
     const { quantity } = body;
 
@@ -65,45 +65,6 @@ export async function PATCH(
     return NextResponse.json(
       {
         error: "Failed to fetch products",
-        details: message,
-      },
-      { status: 500 }
-    );
-  }
-}
-
-/**
- * Get specific item on cart
- */
-
-export async function GET(
-  request: NextRequest,
-  { params }: { params: { id: string } }
-) {
-  try {
-    const res = await fetch(
-      `http://${process.env.PUBLIC_CART_URL}/${params.id}`
-    );
-
-    if (!res.ok) {
-      return NextResponse.json(
-        {
-          error: "Something went wrong!",
-        },
-        { status: 401 }
-      );
-    }
-
-    const data = await res.json();
-
-    return NextResponse.json(data, { status: 200 });
-  } catch (error: unknown) {
-    const message =
-      error instanceof Error ? error.message : "Unexpected error occurred";
-
-    return NextResponse.json(
-      {
-        error: "Failed to fetch item",
         details: message,
       },
       { status: 500 }
